50790ca3e674e6e28f6f2a2a914e4c5550f9d94b
vishnu1002 <vishnuramguru@gmail.com>
2025-08-11 11:06:43 +0530

    Add tree management features and details view
    Updated TreeController to manage a list of trees and added a details view.
    Created MyTree model with properties for tree attributes.
    Modified Index.cshtml to display trees in a table format with links.
    Introduced Details.cshtml to show individual tree details.

--- WebApp_Demo/Controllers/TreeController.cs
+++ WebApp_Demo/Controllers/TreeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_Demo.Models;
 
 namespace WebApp_Demo.Controllers
 {
@@ -6,7 +7,28 @@ namespace WebApp_Demo.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<MyTree> ListofTrees = new List<MyTree>();
+            ListofTrees.Add(new MyTree { Id = 1, Name = "Oak", Description = "A large tree with a thick trunk.", TreeCount = 10 });
+            ListofTrees.Add(new MyTree { Id = 2, Name = "Pine", Description = "A coniferous tree with needle-like leaves.", TreeCount = 5 });
+            ListofTrees.Add(new MyTree { Id = 3, Name = "Maple", Description = "A tree known for its vibrant fall colors.", TreeCount = 8 });
+            ListofTrees.Add(new MyTree { Id = 4, Name = "Birch", Description = "A tree with distinctive white bark.", TreeCount = 6 });
+            ListofTrees.Add(new MyTree { Id = 5, Name = "Cherry", Description = "A tree known for its beautiful blossoms.", TreeCount = 4 });
+
+            ViewData["all_trees"] = ListofTrees;
+
+            return View(ListofTrees);
+        }
+
+        public IActionResult Details()
+        {
+            var tree1 = new MyTree
+            {
+                Id = 1,
+                Name = "Neam",
+                Description = "A large tree with a thick trunk.",
+                TreeCount = 10
+            };
+            return View(tree1);
         }
     }
 }
---
+++ WebApp_Demo/Models/MyTree.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp_Demo.Models
+{
+    public class MyTree
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int TreeCount { get; set; }
+    }
+}
